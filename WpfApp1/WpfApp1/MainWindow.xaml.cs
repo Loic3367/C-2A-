b6bd1b5fbb684ea7ea4ac1b72a9fe44913aca190
SYSINF\adai103 <loic.guldenfels@gmail.com>
2019-01-18 14:15:28 +0100

    Début Projet + Maquettage

---
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            string identifiantInput;
+            string pwdInput;
+
+
+
+            if (String.IsNullOrWhiteSpace(Identifiant.Text) == true)
+            {
+                MessageBox.Show("Veuillez entrer un identifiant");
+                return;
+            }
+            else
+            {
+                identifiantInput = this.Identifiant.Text;
+            }
+            
+            if (String.IsNullOrWhiteSpace(inputPwd.Text) == true)
+            {
+                MessageBox.Show("Veuillez entrer un mot de passe");
+                return;
+            }
+            else {
+                pwdInput = this.inputPwd.Text;
+            }
+            Window1 newWindow = new Window1();
+            newWindow.Show();
+            Application.Current.MainWindow = newWindow;
+            this.Close();
+        }
+    }
+}
